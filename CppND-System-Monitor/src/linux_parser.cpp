124bebd789e5b52711774b9e36678aa084e9d986
srijith <sumakanthil@innovative-logistics.com>
2019-11-01 17:35:46 -0500

    New: Memory utilization (simple)

--- CppND-System-Monitor/src/linux_parser.cpp
+++ CppND-System-Monitor/src/linux_parser.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 
+
 #include "linux_parser.h"
 
 using std::stof;
@@ -67,7 +68,30 @@ vector<int> LinuxParser::Pids() {
 }
 
 // TODO: Read and return the system memory utilization
-float LinuxParser::MemoryUtilization() { return 0.0; }
+float LinuxParser::MemoryUtilization() {
+  float MemTotal, MemFree, MemUsed;
+  string key, value;
+  string line;
+  std::ifstream stream(kProcDirectory + kMeminfoFilename);
+  if (stream.is_open()) 
+  {
+    while(std::getline(stream, line))
+    {
+      std::istringstream linestream(line);
+      linestream >> key >> value;
+      if (key == "MemTotal:") 
+      {
+        MemTotal = std::stof(value);
+      }
+      else if (key == "MemFree:")
+      {
+        MemFree = std::stof(value);
+      }
+    }
+  }
+  MemUsed = MemTotal - MemFree;
+  return MemUsed/MemTotal;
+}
 
 // TODO: Read and return the system uptime
 long LinuxParser::UpTime() { return 0; }
